6614c2dbea017bceea6d681b92bc7a170ea52b5d
disukhii <dsyhiy1234@gmail.com>
2021-01-28 18:10:32 +0200

    infoBlocks

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({ static: './build' });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // порт на якому буде висіти сервер
 
 // server.use('/', express.static(path.join(__dirname, 'build')))
 
@@ -10,3 +10,6 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+
+// тестовий сервер який читає чи є конект користувача з сайтом
